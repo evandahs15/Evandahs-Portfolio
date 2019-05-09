f2a87f8abea22b4948a694a20bff9c36e54288d9
evandahs15 <vandahs@outlook.com>
2019-05-09 23:36:45 +1200

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -121,7 +121,8 @@ function watcher () {
 task('deploy', function () {
   return src('./build/**/*', {read: false})
     .pipe(deploy({
-      repository: 'https://github.com/evandahs15/EvandahsPortfolio.git'
+      repository: 'https://github.com/evandahs15/EvandahsPortfolio.git',
+      branches: ['master']
     }))
 })
 // use default task to launch Browsersync and watch JS files
